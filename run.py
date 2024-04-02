61ab083ad8d85a783e985042911c3c23848c565d
OleksandraMakarets <DELETED-1702562713224>
2024-04-02 16:32:28 +0000

    Add 'fruits' and 'animals' categories

--- run.py
+++ run.py
@@ -2,5 +2,8 @@ import random
 
 # Define categories and words for each category
 categories = {
-    
+    "fruits": ["apple", "banana", "orange", "pear", "grape", "strawberry",
+               "watermelon", "kiwi", "pineapple", "mango"],
+    "animals": ["elephant", "giraffe", "zebra", "rhinoceros", "hippopotamus",
+                "lion", "tiger", "monkey", "crocodile", "penguin"],
 }
