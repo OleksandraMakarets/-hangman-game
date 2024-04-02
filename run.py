7038c9a50e049860266c290fe805246046ba54f9
OleksandraMakarets <DELETED-1702562713224>
2024-04-02 16:33:25 +0000

    Add 'programming_languages' and 'countries' categories

--- run.py
+++ run.py
@@ -6,4 +6,9 @@ categories = {
                "watermelon", "kiwi", "pineapple", "mango"],
     "animals": ["elephant", "giraffe", "zebra", "rhinoceros", "hippopotamus",
                 "lion", "tiger", "monkey", "crocodile", "penguin"],
+    "programming_languages": ["python", "java", "javascript", "ruby", "php",
+                              "csharp", "html", "css", "sql", "swift"],
+    "countries": ["usa", "ireland", "england", "luxembourg", "ukraine",
+                  "brazil", "canada", "germany", "japan", "australia",
+                  "france", "kazakhstan"],
 }
